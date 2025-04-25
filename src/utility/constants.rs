23857f68f509c797a2fecbcfec31f1654341e87d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-04-25 16:09:18 +0100

    refactor(constants): Update Discord emoji

--- src/utility/constants.rs
+++ src/utility/constants.rs
@@ -6,4 +6,4 @@ pub const AFFFILIATES_CHANNEL_ID: ChannelId = ChannelId::new(560895967608569868)
 
 // Emojis.
 pub const FRIENDS_CHAT_EMOJI: &str = "<:friends_chat:805511389337747466>";
-pub const DISCORD_EMOJI: &str = "<:discord:884141994484576277>";
+pub const DISCORD_EMOJI: &str = "<:discord:1365342635228659765>";
