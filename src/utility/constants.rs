3b8daa2894caebed3f3663cc125fecc4ea901d8e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-04-25 16:58:08 +0100

    refactor(constants): Update friends chat emoji

--- src/utility/constants.rs
+++ src/utility/constants.rs
@@ -5,5 +5,5 @@ pub const RAW_AFFILIATES_CHANNEL_ID: ChannelId = ChannelId::new(1265695254107914
 pub const AFFFILIATES_CHANNEL_ID: ChannelId = ChannelId::new(560895967608569868);
 
 // Emojis.
-pub const FRIENDS_CHAT_EMOJI: &str = "<:friends_chat:805511389337747466>";
+pub const FRIENDS_CHAT_EMOJI: &str = "<:friends_chat:1365351046159007786>";
 pub const DISCORD_EMOJI: &str = "<:discord:1365342635228659765>";
